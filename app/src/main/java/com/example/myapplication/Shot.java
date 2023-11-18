9d438204fa1acae9975bbd7b5ba61d55f3513918
JuanAV21 <75455770+JuanAV21@users.noreply.github.com>
2023-11-18 05:47:31 -0500

    progress

---
+++ app/src/main/java/com/example/myapplication/Shot.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Shot {
+    Bitmap shot;
+    Context context;
+    int shx, shy;
+
+    public Shot(Context context, int shx, int shy){
+        this.context = context;
+        shot = BitmapFactory.decodeResource(context.getResources(), R.drawable.astroid);
+        this.shx = shx;
+        this.shy = shy;
+
+    }
+
+    public Bitmap getShot(){
+        return shot;
+    }
+}
